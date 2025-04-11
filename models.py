8283898824411bc037edb02913a3966f89c441af
Srikar K <srikarkovvuri5@gmail.com>
2025-04-10 22:08:54 -0400

    started building out the models

---
+++ models.py
@@ -0,0 +1,48 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    products = db.relationship('Product', backref='seller', lazy=True)
+    reviews = db.relationship('Review', backref='author', lazy=True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    condition = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String(250), nullable=True)
+    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
+    is_available = db.Column(db.Boolean, default=True)
+
+    seller_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+    reviews = db.relationship('Review', backref='product', lazy=True)
+    orders = db.relationship('Order', backref='product', lazy=True)
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    buyer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    ordered_at = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(50), default='Pending')
+
+    buyer = db.relationship('User', backref='orders', foreign_keys=[buyer_id])
